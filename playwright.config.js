669aa0422b54c1906bb5ee552a2308bc61c863d9
Lương Thúy Vy <113878498+LuongThuyVy@users.noreply.github.com>
2026-04-20 21:21:45 +0700

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
     /* Collect trace when retryin
     g the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: process.env.CI ? true : false,
   },
 
   /* Configure projects for major browsers */
